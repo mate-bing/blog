280e0be62dcb668acc539c2477278f4a37c9568a
廖清松 <liaoqing156@gmail.com>
2017-12-16 19:59:54 +0800

    finish the login

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #本项目仿造《跟老齐学python-Django实战》
+
+##第二次提交出现了很大的错误，我的templates/account/login.html运行之后并没有出现表单。所以下次我不得不使用内置的登录和退出方法
--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^$', views.blog_title, name="blog_title") ,
     url(r'(?P<article_id>\d)/$', views.blog_article, name="blog_detail"),
     #在这里并不明确name属性所指意思
+    #构成入口链接，以行使调用，我所知道的调用为HTML中的调用
 
 ]
 
