0ac5ea0643d6d3690e51c2d17ed2d21a2c2347ae
廖清松 <liaoqing156@gmail.com>
2017-12-17 16:50:30 +0800

    finish the login and register

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,19 @@ from django.conf.urls import  url
 from . import  views
 
 
+from  django.conf import  settings
+
+
+from  django.contrib.auth import  views as auth_views
+
+
 urlpatterns=[
-    url(r'^login/$',views.user_login,name="user_login"),
+    #url(r'^login/$',views.user_login,name="user_login"),
+    url(r'^login/$',auth_views.login ,name= 'user_login') ,
+    url(r'^new-login/$',auth_views.login, {"templates_name":"account/login.html"}),
+    url(r'^logout/$' ,auth_views.logout,{"template_name":"account/logout.html"},name='user_logout'),
+    url(r'^register/$',views.register,name="user_register"),
+    url(r'^password-change/$', auth_views.password_change, {"post_change_redirect":"/account/password-change-done"},name='password_change'),
+    url(r'^password-change-done/$', auth_views.password_change_done, name='password_change_done'),
+
 ]
--- website/settings.py
+++ website/settings.py
@@ -124,3 +124,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR,"static"),
 )
+LOGIN_REDIRECT_URL ='/blog/'
